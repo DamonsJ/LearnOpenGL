f9f4afa139eabbc0a108e5246e82a7667dfbcfbb
DamonsJ <shlkl99@163.com>
2023-12-14 10:27:21 +0800

    feat: init commit and pass simple test

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dir=$(pwd)
+echo "Building $dir"
+# rm -rf build
+mkdir -p build
+cmake -B build -S .
+cmake -B build -S .
+cmake --build build
+# cmake --install build --prefix $dir -v
+cmake --install build
